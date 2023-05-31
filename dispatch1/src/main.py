5a27f43f699dcc19a387d4f1af337fe032382524
kabin Hamal <kabin.singh.hamal@kyndryl.com>
2023-05-31 13:30:57 -0400

    repo dispatch1

---
+++ dispatch1/src/main.py
@@ -0,0 +1,51 @@
+import requests
+import json
+import os
+import ast
+
+def main():
+    github_url = "https://api.github.com"
+    org =  str(os.environ["org"])
+    callee_repo_name =  str(os.environ["callee_repo_name"])
+    func_token =  str(os.environ["func_token"])
+    payload =  str(os.environ["payload"])
+    workflow_id = str(os.environ["workflow_id"])
+    branch =  str(os.environ["branch"])
+    
+
+    
+    repo_dispatch(payload, workflow_id, github_url, org, callee_repo_name, branch, func_token)
+    
+    
+    
+    
+
+def repo_dispatch(payload, workflow_id, github_url, org, callee_repo_name, branch, func_token):
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {func_token}",
+    }
+
+    data = {
+        "event_type": f"{workflow_id}",
+        "client_payload": {
+            "payload": payload,
+            "branch": branch
+        }
+    }
+
+    response = requests.post(
+        f"{github_url}/repos/{org}/{callee_repo_name}/dispatches",
+        json=data,
+        headers=headers
+    )
+
+    if response.status_code == 204:
+        print(f"Successfully triggered repo_dispatch on the repo: {callee_repo_name} with event_type: {workflow_id} and branch: {branch}")
+    else:
+        print(f"Repo_dispatch on the repo: {callee_repo_name} failed")
+
+
+
+if __name__ == "__main__":
+    main()
